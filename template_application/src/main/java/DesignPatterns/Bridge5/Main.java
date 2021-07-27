cfb0a69c58f550a6adba429141a7d12b219f303b
akhachatryan@universe.dart.spb <asya.khachatryan@dataart.com>
2021-07-27 11:42:05 +0400

    Functional Interfaces

--- template_application/src/main/java/DesignPatterns/Bridge5/Main.java
+++ template_application/src/main/java/DesignPatterns/Bridge5/Main.java
@@ -2,7 +2,8 @@ package DesignPatterns.Bridge5;
 
 public class Main {
     public static void main(String[] args) {
-        Shape shape= new LIne(new OpenGlDraw());
+        DrawApi drawApi= new OpenGlDraw();
+        Shape shape= new LIne(drawApi);
         shape.draw();
         System.out.println(shape);
     }
